7cc3fe9dfd5fef37959efac85743b264a73e20cb
deadcoffeecup <tsikora555@gmail.com>
2022-06-03 23:41:39 +0200

    Checking rows with Set obj

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Square from './Square';
-
 class Table extends React.Component {
   constructor() {
     super();
@@ -8,9 +7,10 @@ class Table extends React.Component {
       squares: Array(81).fill(0),
     };
     this.handleChange = this.handleChange.bind(this);
-    this.solve = this.solve.bind(this);
+    this.isValid = this.isValid.bind(this);
   }
-  solve() {
+
+  isValid() {
     let board = [
       // [3, 1, 6, 5, 7, 8, 4, 9, 2],
       // [5, 2, 9, 1, 3, 4, 7, 6, 8],
@@ -21,26 +21,18 @@ class Table extends React.Component {
       // [1, 3, 8, 9, 4, 7, 2, 5, 6],
       // [6, 9, 2, 3, 5, 1, 8, 7, 4],
       // [7, 4, 5, 2, 8, 6, 3, 1, 9],
+      //that's solved one
     ];
     for (let i = 0; i < 9; i++) {
       board[i] = [];
       board[i] = [...this.state.squares.slice(9 * i, 9 + 9 * i)];
     }
-    let isRowsValid = 0;
-    let isColsValid = 0;
+    let isRowsValid = true;
     for (let i = 0; i < 9; i++) {
-      if (board[i].reduce((a, b) => a + b) === 45) isRowsValid = 1;
+      isRowsValid = isRowsValid && board[i].length === new Set(board[i]).size;
     }
-    let valueCols = 0;
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        valueCols = valueCols + board[j][i];
-      }
-      if (valueCols === 45) isColsValid++;
-      valueCols = 0;
-    }
-    isColsValid = !isColsValid ? 0 : 1;
-    if (isRowsValid === 1 && isColsValid === 1) {
+    //Checking only rows
+    if (isRowsValid) {
       alert('Udało się!');
     } else {
       alert('Próbuj dalej!');
@@ -56,6 +48,7 @@ class Table extends React.Component {
       ],
     }));
   }
+
   renderSquares(i) {
     let all = [];
     for (let i = 0; i < 81; i++) {
@@ -71,7 +64,7 @@ class Table extends React.Component {
       <div className='row'>
         {this.renderSquares(81)}
 
-        <button onClick={this.solve}>Check!</button>
+        <button onClick={this.isValid}>Check!</button>
       </div>
     );
   }
