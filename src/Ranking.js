78f33bfc0a88ac1430d77a3b7acc6c54253203fd
deadcoffeecup <tsikora555@gmail.com>
2022-06-17 01:34:41 +0200

    convert sec to H:M:S format and  files cleanup

--- src/Ranking.js
+++ src/Ranking.js
@@ -1,4 +1,12 @@
 export const Ranking = ({ ranking }) => {
+  const convertSecToSecAndMin = (time) => {
+    let hours = Math.floor(time / 3600);
+    time = time - hours * 3600;
+    let minutes = Math.floor(time / 60);
+    let seconds = time - minutes * 60;
+    return `Time: ${hours}:${minutes}:${seconds}`;
+  };
+
   return (
     <ul className='ranking-list'>
       <h3>Winners:</h3>
@@ -6,7 +14,7 @@ export const Ranking = ({ ranking }) => {
         <li key={el.userName}>
           {el.userName}
           <ul>
-            <li> {el.timeDone}</li>
+            <li> {convertSecToSecAndMin(el.timeDone)}</li>
           </ul>
         </li>
       ))}
